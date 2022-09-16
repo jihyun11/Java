c2c84f72eb6de2e2b15d74b6b1a354ce758bd532
지현 <372491@naver.com>
2022-09-16 22:22:25 +0900

    subString()

---
+++ instance.java
@@ -0,0 +1,13 @@
+public class instance {
+
+    public static void main(String[] args) {
+
+        String ls = "abcdef";
+        instance is = new instance(); //??객체 이름은 클래스 이름이랑 같아야 하나? 여기ㅔㅇ서는 객체가 필요 없는것?
+
+
+        System.out.println(ls.substring(1, 4));
+
+    }
+
+}
