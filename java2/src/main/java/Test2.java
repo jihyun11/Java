3c830c04f4f2db459d778454e5ca36d3bdd23b55
지현 <372491@naver.com>
2022-09-30 22:10:43 +0900

    codewars vowel count 문제 안풀려서 인텔리제이에 해봄

--- java2/src/main/java/Test2.java
+++ java2/src/main/java/Test2.java
@@ -1,35 +1,18 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
-
 public class Test2 {
-    public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
+    public static int main(String[] args) {
+        String str = "asfsdfasfasfs";
+        char[] alpha = {'a', 'e', 'i', 'u', 'o'};
 
-        list.add(1);
-        list.add(1);
-        list.add(1);
-        list.add(1);
-        list.add(2);
-        list.add(2);
-        list.add(2);
-        list.add(3);
-        list.add(3);
-        list.add(3);
+            int sum = 0;
+            for (int i = 0; i < str.length(); i++) {
+                if ((str.charAt(i)=='a') || (str.charAt(i)=='e') ||
+                        (str.charAt(i)=='i') ||
+                        (str.charAt(i)=='o') || (str.charAt(i)=='u')) {
+                    sum++;
+                }
 
-        HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
-        for (int i = 0; i < list.size(); i++) {
-            if (hashMap.containsKey((list.get(i)))) {
-                hashMap.put(list.get(i), hashMap.get(list.get(i) +1 ));
-            } else {
-                hashMap.put(list.get(i), 1);
-            }
-            Set<Integer> keys = hashMap.keySet();
-            for (int key : keys) {
-                System.out.println(key + " : " + hashMap.get(key) + "개");
-            }
+            } return sum;
         }
     }
 
-}
+
