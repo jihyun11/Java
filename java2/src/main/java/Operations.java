0b5d768b657d797ad3b4293be2b751ee90c1a53d
지현 <372491@naver.com>
2022-10-10 20:03:50 +0900

    Basic Operations 인텔리제이

---
+++ java2/src/main/java/Operations.java
@@ -0,0 +1,16 @@
+public class Operations {
+    public static void main(String[] args) {
+        String op = "+";
+        int v1 = 4;
+        int v2 = 5;
+
+        if (op == "+") {
+            System.out.println(v1 + v2);
+        }
+
+
+        }
+
+
+    }
+
