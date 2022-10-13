4549f2c89764c6183dfa812ad670a192568fa23f
지현 <372491@naver.com>
2022-10-13 16:58:24 +0900

    인터페이스로 @override 후 업캐스팅

---
+++ java2/src/main/java/ObjectGrouping.java
@@ -0,0 +1,17 @@
+public class ObjectGrouping {
+    public static void main(String[] args) {
+
+        Flyable.Bird bird = new Flyable.Bird();
+        Flyable.Helicopter copter = new Flyable.Helicopter();
+        Flyable.Rocket rocket = new Flyable.Rocket();
+
+        // 인터페이스 타입으로 그룹화
+        Flyable[] flyableThings = { bird, copter, rocket };
+
+        // 모든 날것들을 날림
+        for (int i = 0; i < flyableThings.length; i++) {
+            Flyable temp = flyableThings[i];
+            temp.fly();
+        }
+    }
+}
