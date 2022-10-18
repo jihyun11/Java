a1e81a8018ab4894fbcad38accb679bc798dcc88
지현 <372491@naver.com>
2022-10-18 16:49:41 +0900

    Square Every Digit 인텔리제이 Stream.of(), String.valueOf(), .mapToInt(Integer::parseInt), toArray()

--- java2/src/main/java/SquareEveryDigit.java
+++ java2/src/main/java/SquareEveryDigit.java
@@ -1,18 +1,16 @@
-import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class SquareEveryDigit {
     public static void main(String[] args) {
         int n = 81118;
 
-        int[] array = new int[200];
+        int[] sd = Stream.of(String.valueOf(n).split("")).mapToInt(Integer::parseInt).toArray(); // String 배열로 만들어서 넣었다가, 다시 int 배열로 형변환
+        String sum = "";
+        for (int i = 0; i < sd.length; i++) {
+            sum += Integer.toString(sd[i] * sd[i]); // 원하는 값 계산 후 합쳐지지 않게 (숫자니까 더해져버림) String으로 다시 형변환
 
-        String temp = Integer.toString(n);
-        int[] digit = new int[temp.length()];
-//        for (int i = 0; i < temp.length(); i++)  {
-//            digit[i] = temp.charAt(i) - '0';
-//            int[] nums = Arrays.stream(digit).mapToDouble(Integer::parseInt).toArray();
-//            double result = Math.pow(nums[], 2);
-//
-//        }  System.out.println(Arrays.toString(nums));
+        }
+        int summ1 = Integer.parseInt(sum); // 다시 숫자형태로 만들어서 출력
+        System.out.println(summ1);
     }
 }
