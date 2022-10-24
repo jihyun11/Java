d6deeb2ceb69a9b2a331a73705ad660af060a356
지현 <372491@naver.com>
2022-10-24 22:08:39 +0900

    Shortest word (문자열 배열에 넣기, 배열 중 첫 번째 요소 고정 후 for문으로 비교, if문 안에서 비교 값 재할당)

---
+++ java2/src/main/java/ShortestWord.java
@@ -0,0 +1,19 @@
+public class ShortestWord {
+    public static void main(String[] args) {
+        String s = "bitcoin take over the world maybe who knows perhaps";
+
+        String[] words = s.split(" ");
+        int small = words[0].length();
+
+        for(int i = 0; i < words.length; i++) {
+            if (small > words[i].length()) {
+                small = words[i].length();
+
+            }
+        }
+        System.out.println(small);
+
+
+    }
+}
+
