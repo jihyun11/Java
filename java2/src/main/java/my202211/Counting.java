0ff4d26eedb2682a0584078c16a8a6987b3776ed
지현 <372491@naver.com>
2022-12-30 21:11:20 +0900

    no message

--- java2/src/main/java/my202211/Counting.java
+++ java2/src/main/java/my202211/Counting.java
@@ -1,19 +1,36 @@
 package my202211;
 
+import java.util.*;
+
 public class Counting {
     public static void main(String[] args) {
-        String text = "abaB";
-        String s = text.toLowerCase();
-        int ans = 0;
+        int[] a = {1, 2};
 
-        while (text.length() > 0) {
-            String firstLetter = text.substring(0, 1);
-            text = text.substring(1);
-            if(text.contains(firstLetter)) {
-                ans ++;
-                text = text.replace(firstLetter, "");
-            }
-            System.out.println(ans);
-        }
+
+
+        List<Integer> first = Arrays.asList(a[0], a[1]);
+        List<Integer> second = Arrays.asList(2, 3);
+
+        List<Integer> diff = new ArrayList<>(first);
+        diff.removeAll(second);
+
+        System.out.println(diff);
+
+
+        int[] b = {2};
+
+
+//        Set<int[]> setA = new HashSet<>(Arrays.asList(a));
+//        Set<int[]> setB = new HashSet<>(Arrays.asList(b));
+//
+//        Set<int[]> setC = new HashSet<>();
+//        setA.removeAll(setB);
+//
+//
+//        System.out.println(setA.toString());
+
+
     }
+
 }
+
