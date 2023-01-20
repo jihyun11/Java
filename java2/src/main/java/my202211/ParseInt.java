1260b3db3a50a9ab30290d76b0d711508f9c7192
지현 <372491@naver.com>
2023-01-20 16:05:09 +0900

    배열을 포함한 형변환 하기 (Integer.ParseInt(), String.valueOf())

---
+++ java2/src/main/java/my202211/ParseInt.java
@@ -0,0 +1,24 @@
+package my202211;
+
+public class ParseInt {
+    public static void main(String[] args) {
+        String num = "";
+        int number = 153;
+
+        
+
+//        num = String.valueOf(number);
+//        String[] numarray = num.split("");
+//        int[] numberarray = {numarray.length};
+//        int full = 0;
+//        int full2 = 0;
+//
+//        for (int i = 0; i < numarray.length; i++) {
+//            full += Integer.valueOf((Integer.parseInt(numarray[i])) * Integer.parseInt(numarray[i]));
+//
+//
+//        }
+//        System.out.println(full);
+
+    }
+}
