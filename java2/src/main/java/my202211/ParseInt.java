7a2aa346a14a1a04eefb7f7db245aae293c82650
지현 <372491@naver.com>
2023-01-25 03:37:02 +0900

    스택 객체를 활용한 문자열 구분하기

--- java2/src/main/java/my202211/ParseInt.java
+++ java2/src/main/java/my202211/ParseInt.java
@@ -1,24 +1,29 @@
 package my202211;
 
+import java.util.EmptyStackException;
+import java.util.Stack;
+
 public class ParseInt {
     public static void main(String[] args) {
-        String num = "";
-        int number = 153;
-
-        
-
-//        num = String.valueOf(number);
-//        String[] numarray = num.split("");
-//        int[] numberarray = {numarray.length};
-//        int full = 0;
-//        int full2 = 0;
-//
-//        for (int i = 0; i < numarray.length; i++) {
-//            full += Integer.valueOf((Integer.parseInt(numarray[i])) * Integer.parseInt(numarray[i]));
-//
-//
-//        }
-//        System.out.println(full);
+        //스택을 활용한 문자열 구분하기
+        String braces = "(){}[]";
+        char item = 0;
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < braces.length(); i++) {
+            char c = braces.charAt(i);
+            if (c == '(' || c == '{' || c == '[') st.push(c);
+            else {
+                try {
+                    item = st.pop();
+                } catch (EmptyStackException e) {
+                    System.out.println("False");
+                }
+                if ((c == ')' && item != '(') || (c == '}' && item != '{' || (c == ']' && item != '['))) {
+                    System.out.println("Fasle");
+                }
+            }
+        }
+        System.out.println(st.isEmpty());
 
     }
 }
