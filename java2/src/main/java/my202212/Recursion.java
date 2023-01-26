76d910fa7dd24c1bead23bdbfa3a5d39d9ee7af8
지현 <372491@naver.com>
2023-01-27 00:02:30 +0900

    재귀함수를 이용해 특정 숫자까지의 합 구하기 (변수의 중첩)

--- java2/src/main/java/my202212/Recursion.java
+++ java2/src/main/java/my202212/Recursion.java
@@ -2,9 +2,11 @@ package my202212;
 
 public class Recursion {
     public static void main(String[] args) {
+        int num = 0;
         Fuction(5);
+        System.out.println(Sum(5));
     }
-    public static int Fuction(int number) {
+    public static int Fuction(int number) { //문자열을 반복해서 출력하는 재귀함수
 
         if (number == 0) {
             return 0;
@@ -12,6 +14,18 @@ public class Recursion {
         else {
             System.out.println("Hello world");
             return Fuction(number - 1);
+        }
+    }
+
+    public static int Sum(int sum) { //0부터 5까지의 합을 구하는 재귀함수
+
+        if (sum == 0) {
+            return 0;
+        }
+        else {
+
+
+            return sum + Sum(sum - 1);
         }
     }
 }
