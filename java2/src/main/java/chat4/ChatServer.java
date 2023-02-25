8c0a42e6d480fad6d9d7d7f8b3a210d56f771383
지현 <372491@naver.com>
2023-02-25 11:02:12 +0900

    일대일 채팅 프로그램 (스레드 포함)

---
+++ java2/src/main/java/chat4/ChatServer.java
@@ -0,0 +1,34 @@
+package chat4;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatServer {
+    void listen() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(9999);
+
+        try {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                OutputStream outputStream = socket.getOutputStream();
+                PrintWriter printWriter = new PrintWriter(outputStream);
+
+
+                while (true) {
+                    String msg = bufferedReader.readLine();
+
+                    System.out.println(msg);
+
+                    printWriter.println(msg);
+                    printWriter.flush();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ java2/src/main/java/chat4/Client.java
@@ -0,0 +1,41 @@
+package chat4;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 9999);
+        Scanner scanner = new Scanner(System.in);
+
+        OutputStream outputStream = socket.getOutputStream();
+        PrintWriter printWriter = new PrintWriter(outputStream);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                    while (true) {
+                        String msg = bufferedReader.readLine();
+
+                        System.out.println(msg);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+
+        while (true) {
+            String message = scanner.nextLine();
+
+            printWriter.println(message);
+            printWriter.flush();
+        }
+
+    }
+}
