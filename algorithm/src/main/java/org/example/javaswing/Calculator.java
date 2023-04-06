ee42c362ee6a7e37756ee9585484460c486b0d43
jihyun <372491@naver.com>
2023-04-06 22:26:54 +0900

    자바 스윙 - 계산기 모양 디자인

--- algorithm/src/main/java/org/example/javaswing/Calculator.java
+++ algorithm/src/main/java/org/example/javaswing/Calculator.java
@@ -1,6 +1,8 @@
 package org.example.javaswing;
 
 import javax.swing.*;
+import javax.swing.text.Document;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,15 +13,46 @@ public class Calculator extends JFrame implements ActionListener {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
         setLocationRelativeTo(null);
-//        setLayout(new FlowLayout());
+        setLayout(new FlowLayout());
 
-        JLabel label = new JLabel("0", JLabel.RIGHT);
-        JPanel btnView = new JPanel();
+        JTextField jf = new JTextField("", 20);
+        add(jf);
+        JButton jButton = new JButton("1");
+        add(jButton);
+        JButton jButton2 = new JButton("2");
+        add(jButton2);
+        JButton jButton3 = new JButton("3");
+        add(jButton3);
+        JButton jButton4 = new JButton("4");
+        add(jButton4);
+        JButton jButton5 = new JButton("5");
+        add(jButton5);
+        JButton jButton6 = new JButton("6");
+        add(jButton6);
+        JButton jButton7 = new JButton("7");
+        add(jButton7);
+        JButton jButton8 = new JButton("8");
+        add(jButton8);
+        JButton jButton9 = new JButton("9");
+        add(jButton9);
 
-//        btnView.setLayout(new GroupLayout(5, 3, 2, 2));
+        JButton jButton0 = new JButton("0");
+        add(jButton0);
+        JButton jButtonP = new JButton("+");
+        add(jButtonP);
+        JButton jButtonM = new JButton("-");
+        add(jButtonM);
+        JButton jButtonMul = new JButton("*");
+        add(jButtonMul);
+        JButton jButtonNa = new JButton("/");
+        add(jButtonNa);
+        JButton jButtonRe = new JButton("=");
+        add(jButtonRe);
+        JButton jButtonCan = new JButton("AC");
+        add(jButtonCan);
 
-        JButton btn = new JButton("1");
-        add(btn);
+
+
 
         setVisible(true);
     }
