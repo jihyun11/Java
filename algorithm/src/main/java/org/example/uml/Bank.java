f2afd5f0b8d1db4b4b743b5bf538727e27dc61ed
jihyun <372491@naver.com>
2023-04-08 11:23:26 +0900

    uml Bank 클래스 추가하기

---
+++ algorithm/src/main/java/org/example/uml/Bank.java
@@ -0,0 +1,23 @@
+package org.example.uml;
+
+import java.util.ArrayList;
+
+public class Bank {
+
+    ArrayList<User> users = new ArrayList<>();
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public boolean updateUser(User user) {
+        for (int i = 0; i < users.size(); i++) {
+            if (users)
+        }
+    }
+
+    public boolean deleteUser(String id) {
+        return true;
+    }
+
+}
