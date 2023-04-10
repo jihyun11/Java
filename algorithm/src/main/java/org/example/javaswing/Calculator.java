905feff8274aa358314ad8a8660b8bdfb0eb0c3a
jihyun <372491@naver.com>
2023-04-11 00:00:56 +0900

    자바 스윙 - Calculator 레이아웃, 액션 리스너 수정

--- algorithm/src/main/java/org/example/javaswing/Calculator.java
+++ algorithm/src/main/java/org/example/javaswing/Calculator.java
@@ -8,6 +8,8 @@ import java.awt.event.ActionListener;
 
 public class Calculator extends JFrame implements ActionListener {
 
+    private JButton jButton;
+
     public Calculator(String title) {
         setTitle(title);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -15,41 +17,51 @@ public class Calculator extends JFrame implements ActionListener {
         setLocationRelativeTo(null);
         setLayout(new FlowLayout());
 
+
+
         JTextField jf = new JTextField("", 20);
         add(jf);
-        JButton jButton = new JButton("1");
-        add(jButton);
-        JButton jButton2 = new JButton("2");
-        add(jButton2);
-        JButton jButton3 = new JButton("3");
-        add(jButton3);
-        JButton jButton4 = new JButton("4");
-        add(jButton4);
-        JButton jButton5 = new JButton("5");
-        add(jButton5);
-        JButton jButton6 = new JButton("6");
-        add(jButton6);
-        JButton jButton7 = new JButton("7");
-        add(jButton7);
-        JButton jButton8 = new JButton("8");
-        add(jButton8);
-        JButton jButton9 = new JButton("9");
-        add(jButton9);
 
-        JButton jButton0 = new JButton("0");
-        add(jButton0);
-        JButton jButtonP = new JButton("+");
-        add(jButtonP);
-        JButton jButtonM = new JButton("-");
-        add(jButtonM);
-        JButton jButtonMul = new JButton("*");
-        add(jButtonMul);
-        JButton jButtonNa = new JButton("/");
-        add(jButtonNa);
-        JButton jButtonRe = new JButton("=");
-        add(jButtonRe);
-        JButton jButtonCan = new JButton("AC");
-        add(jButtonCan);
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new GridLayout(3, 3));
+
+        jButton = new JButton("1");
+        jButton.addActionListener(this);
+        jPanel.add(jButton);
+
+        JButton jButton2 = new JButton("2");
+        jPanel.add(jButton2);
+
+        JButton jButton3 = new JButton("3");
+        jPanel.add(jButton3);
+        JButton jButton4 = new JButton("4");
+        jPanel.add(jButton4);
+        JButton jButton5 = new JButton("5");
+        jPanel.add(jButton5);
+        JButton jButton6 = new JButton("6");
+        jPanel.add(jButton6);
+        JButton jButton7 = new JButton("7");
+        jPanel.add(jButton7);
+        JButton jButton8 = new JButton("8");
+        jPanel.add(jButton8);
+        JButton jButton9 = new JButton("9");
+        jPanel.add(jButton9);
+
+        add(jPanel);
+//        JButton jButton0 = new JButton("0");
+//        add(jButton0);
+//        JButton jButtonP = new JButton("+");
+//        add(jButtonP);
+//        JButton jButtonM = new JButton("-");
+//        add(jButtonM);
+//        JButton jButtonMul = new JButton("*");
+//        add(jButtonMul);
+//        JButton jButtonNa = new JButton("/");
+//        add(jButtonNa);
+//        JButton jButtonRe = new JButton("=");
+//        add(jButtonRe);
+//        JButton jButtonCan = new JButton("AC");
+//        add(jButtonCan);
 
 
 
@@ -63,6 +75,11 @@ public class Calculator extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        Object obj = e.getSource();
+
+        if (obj == jButton) {
+            System.out.println("1 누름");
+        }
 
     }
 }
