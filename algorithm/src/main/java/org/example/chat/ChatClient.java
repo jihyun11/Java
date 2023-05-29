5434e9af4596baf66fa3d176a9d2adcca99f7940
jihyun <372491@naver.com>
2023-05-29 18:28:10 +0900

    1:1 채팅방 프로그램 코드 (서버, 클라이언트)

---
+++ algorithm/src/main/java/org/example/chat/ChatClient.java
@@ -0,0 +1,47 @@
+package org.example.chat;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ChatClient {
+    public static void main(String[] args) {
+
+        Socket socket = null;
+        BufferedReader in = null;
+        BufferedWriter out = null;
+        Scanner scan = new Scanner(System.in);
+        try {
+
+            socket = new Socket("localhost", 9999);
+
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            while (true) {
+
+                System.out.println("보내기 >> ");
+                String outMsg = scan.nextLine();
+                out.write(outMsg + "\n");
+                out.flush();
+
+                String inMsg = in.readLine();
+                System.out.println(inMsg);
+
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                out.close();
+                in.close();
+                scan.close();
+                socket.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
---
+++ algorithm/src/main/java/org/example/chat/ChatServer.java
@@ -0,0 +1,48 @@
+package org.example.chat;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ChatServer {
+    public static void main(String[] args) {
+        ServerSocket listener = null;
+        Socket socket = null;
+        BufferedReader in = null;
+        BufferedWriter out = null;
+        Scanner scan = new Scanner(System.in);
+        try {
+            listener = new ServerSocket(9999);
+            System.out.println("연결을 기다립니다...");
+            socket = listener.accept();
+            System.out.println("연결이 되었습니다.");
+
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            while (true) {
+                String inMsg = in.readLine();
+                System.out.println(inMsg);
+
+                System.out.println("보내기 >> ");
+                String outMsg = scan.nextLine();
+                out.write(outMsg + "\n");
+                out.flush();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                out.close();
+                in.close();
+                scan.close();
+                socket.close();
+                listener.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
